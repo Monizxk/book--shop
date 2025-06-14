d0730451c2e6f262d8683c1c17c695c5598b29a1
Mariia <desperateope2017@gmail.com>
2025-06-14 13:59:05 +0200

    fix

--- app/Http/Controllers/Api/SettingsController.php
+++ app/Http/Controllers/Api/SettingsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Setting;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class SettingsController extends Controller
 {
@@ -17,7 +18,8 @@ class SettingsController extends Controller
 
     public function getDeliveryCost()
     {
-        $cost = Setting::getValue('delivery_cost', 50);
+        $cost = Setting::getValue('delivery_cost', 250);
+        Log::info('Delivery cost from settings: ' . $cost);
         return response()->json([
             'delivery_cost' => (float) $cost
         ]);
